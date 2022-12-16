75987d767a95a6e3caa863384be9df939a5ea4b6
Nick Hafkemeyer <mail@nhafkemeyer.com>
2022-12-16 15:30:12 +0100

    day 6 part two

--- day6/datastream.py
+++ day6/datastream.py
@@ -2,18 +2,18 @@ import os
 import sys
 
 MARKER_LENGTH = 4
+MESSAGE_MARKER_LENGTH = 14
 
 def parse_input(filename):
     with open(filename, 'r') as f:
         return list(f.readline())
 
 
-def get_marker_pos(datastream):
-    marker = set()
+def get_marker_pos(datastream, marker_length):
     for i in range(len(datastream)):
-        marker = set(datastream[i:i+MARKER_LENGTH])
-        if len(marker) == MARKER_LENGTH:
-            return i + MARKER_LENGTH
+        marker = set(datastream[i:i+marker_length])
+        if len(marker) == marker_length:
+            return i + marker_length
 
 
 if __name__ == '__main__':
@@ -26,4 +26,7 @@ if __name__ == '__main__':
         raise ValueError('File does not exist.')
 
     datastream = parse_input(filename)
-    print(get_marker_pos(datastream))
+    first_start_marker = get_marker_pos(datastream, MARKER_LENGTH)
+    first_message_marker = get_marker_pos(datastream, MESSAGE_MARKER_LENGTH)
+    print(f'The first start-of-packet marker is complete after {first_start_marker} characters.')
+    print(f'The first start-of-message marker is complete after {first_message_marker} characters.')
